987c54b74575ad0c7e8b223a3f7e025f239d919e
unity-creators <55509044+unity-creators@users.noreply.github.com>
2019-10-16 18:02:18 +0530

    Add files via upload

---
+++ fps_character_controller_helper.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Axis
+{
+
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+
+
+}
